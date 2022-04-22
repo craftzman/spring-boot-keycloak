40f9c5fdf845751e6dc5eba62b62d2d02ce9a37f
craftzman <chaixxv@gmail.com>
2022-04-22 20:20:32 +0700

    first initial implementation

---
+++ src/main/java/org/zot/chai/springbootkeycloak/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,6 @@
+package org.zot.chai.springbootkeycloak.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+}
